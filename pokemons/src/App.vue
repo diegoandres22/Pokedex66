b4118cd66398243522ec7ec0dc687cf227961324
diegoandres22 <diego.a.v3005@gmail.com>
2023-09-06 20:03:06 -0300

    landing's code

--- pokemons/src/App.vue
+++ pokemons/src/App.vue
@@ -1,15 +1,16 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Landing />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Landing from './components/Landing/Landing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
@@ -20,7 +21,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+* {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 </style>
---
+++ pokemons/src/components/Landing.vue/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="landing-content">
+
+        <img alt="Pikachu" src="../../assets/pikachuEdit.png">
+        <h3>Welcome to Pokédex</h3>
+        <h5>The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the Pokémon world.</h5>
+        <button>Get started</button>
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'App',
+    components: {
+
+    }
+}
+</script>
+
+<style>
+
+@import './Landing.css'
+</style>
