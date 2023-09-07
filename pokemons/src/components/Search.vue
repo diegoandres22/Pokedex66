c1278276056184660a60e9828f6f017c0e041288
diegoandres22 <diego.a.v3005@gmail.com>
2023-09-07 01:38:01 -0300

    list, search and switch

---
+++ pokemons/src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="site-content">
+
+        <input class="searchbar" placeholder="Search" type="text">
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: 'SearchComponent',
+    components: {
+
+    }
+}
+</script>
+
+<style>
+@import '../assets/Search.css'
+</style>
--- pokemons/src/components/Site.vue
+++ pokemons/src/components/Site.vue
@@ -1,20 +1,65 @@
 <template>
-    <div class="site-content">
+    <Search></Search>
 
-        <router-link to="/">salir</router-link>
-        <br>
-        <input placeholder="Search" type="text">
+    <div v-if="this.switch" class="list">
+        <p v-for=" a  in  pokemons.results " :key="a">{{ a['name'] }} </p>
+
+
+        <div class="paginate">
+            <button>back</button>
+            <b>1</b>
+            <b>2</b>
+            <b>3</b>
+            <button>Next</button>
+        </div>
+
+    </div>
+    <div v-if="!this.switch" class="list">
+        <!-- <p v-for=" a  in  pokemonsFavorites.results " :key="a">{{ a['name'] }} </p> -->
+        <h1>favorites</h1>
+    </div>
+
+    <div class="buttons">
+        <button @click="switchF">All </button>
+        <button @click="switchT">Favorites</button>
     </div>
 </template>
   
 <script>
 
-export default {
-    name: 'SiteComponent',
-    components: {
+import Search from "./Search.vue";
 
+export default {
+    name: 'SearchComponent',
+    mounted() {
+        this.getPokemons()
+    },
+    data() {
+        return {
+            switch: 0,
+            pokemons: [],
+            pokemonsFavorites: []
+        }
+    },
+    methods: {
+        getPokemons: async function () {
+            this.pokemons = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20').then((res) => res.json())
+            console.log(this.pokemons);
+
+        },
+        switchF() {
+            this.switch = 1
+        },
+        switchT() {
+            this.switch = 0
+        }
+    },
+
+    components: {
+        Search
     }
 }
+
 </script>
 
 <style>
